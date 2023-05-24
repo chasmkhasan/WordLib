10e0bef9f3dbfcfd98d9e6771625f39544bda749
MD KAMRUL HASAN <114093612+chasmkhasan@users.noreply.github.com>
2023-05-24 17:51:25 +0200

    Palindrom.View Updated

---
+++ Palindrome.View/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Palindrome.Logic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text to see if its palindrome or not");
+            string text = Console.ReadLine();
+
+            Palindrome palindrome = new Palindrome();
+
+            Console.WriteLine($"The result is {palindrome.IsPalindrome(text).ToString()}");
+            Console.ReadLine();
+        }
+    }
+
+}
